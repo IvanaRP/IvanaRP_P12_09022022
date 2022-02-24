71a9e70fb0a939f1154cc96c9fafff14a7bef48e
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-02-24 19:38:41 +0100

    Update index.jsx
    fetch data

--- sportsee/src/pages/Api/index.jsx
+++ sportsee/src/pages/Api/index.jsx
@@ -1,3 +1,42 @@
+// import React,{useState,useEffect} from 'react';
+
+
+// function Api() {
+//   const [data,setData]=useState([]);
+//   const getData=()=>{
+//     fetch('http://localhost:3000/user/12'
+//     ,{
+//       headers : { 
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//        }
+//     }
+//     )
+//       .then(function(response){
+//         console.log(response)
+//         return response.json();
+//       })
+//       .then(function(myJson) {
+//         console.log(myJson);
+//         setData(myJson)
+//       });
+//   }
+//   useEffect(()=>{
+//     getData()
+//   },[])
+
+//   return (
+//     <div className="App">
+//      {
+//        data && data.length>0 && data.map((item)=><p>{item.userInfos.firstName}</p>)
+//      }
+//     </div>
+//   );
+// }
+
+// export default Api;
+
+
 import React, { Component } from "react";
 
 class Api extends Component {
@@ -12,13 +51,13 @@ class Api extends Component {
     //     .then((data) => console.log(data));
 
     try {
-      const response = await fetch("http://localhost:3000/user/12");
+      const response = await fetch(`http://localhost:3000/user/12`);
     //   const response = await fetch(
     //     "https://jsonplaceholder.typicode.com/posts"
     //   );
       const data = await response.json();
       console.log(response);
-      console.log(data);
+      console.log(JSON.stringify(data));
       this.setState({ data, isLoaded: true });
     } catch (error) {
       console.log(error);
@@ -30,19 +69,39 @@ class Api extends Component {
 
     console.log(this.state);
 
-    const { data, isLoaded } = this.state;
+
+    
+
+    const { isLoaded } = this.state;
+    console.log(this.state.data.data);
+
+const info = this.state.data.data;
+
     return (
       <div>
         app component
         {!isLoaded ? (
           <div> Loading ...</div>
-        ) : (
-          <ul>
-            {data.map((item) => {
-            //   return <li key={item.id}>{item.body}</li>;
-                return <li key={item.id}>{item.data}</li>;
-            })}
-          </ul>
+        ) : ( 
+        <div> 
+          <p>{info.id} </p>
+          <p>{info.todayScore} </p>
+          <p>{info.keyData.calorieCount} </p>
+          <p>{info.keyData.carbohydrateCount} </p>
+          <p>{info.keyData.lipidCount} </p>
+          <p>{info.userInfos.age} </p>
+          <p>{info.userInfos.firstName} </p>
+          <p>{info.userInfos.lastName} </p>
+        </div>
+
+          
+          // <ul>
+           
+          //   { indo && info.map((item) => {
+          //   //   return <li key={item.id}>{item.body}</li>;
+          //       return <li key={item.id}>{item.data}</li>;
+          //   })}
+          // </ul>
         )}
       </div>
     );
