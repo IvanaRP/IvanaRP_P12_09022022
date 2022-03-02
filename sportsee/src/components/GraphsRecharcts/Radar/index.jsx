188eaddb439e03fa5be7527088ed9e6aa3ba43ea
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-03-02 20:00:21 +0100

    radar graphs
    graphPerformance

--- sportsee/src/components/GraphsRecharcts/Radar/index.jsx
+++ sportsee/src/components/GraphsRecharcts/Radar/index.jsx
@@ -9,58 +9,99 @@ import {
 
 import "../../../styles/radar.css";
 
-const data = [
-  {
-    subject: "intensity",
-    value: 90,
-    kind: 6,
-  },
-  {
-    subject: "speed",
-    value: 200,
-    kind: 5,
-  },
-  {
-    subject: "strength",
-    value: 50,
-    kind: 4,
-  },
-  {
-    subject: "endurance",
-    value: 120,
-    kind: 2,
-  },
-  {
-    subject: "energy",
-    value: 80,
-    kind: 1,
-  },
-  {
-    subject: "cardio",
-    value: 200,
-    kind: 1,
-  },
-];
+// const data = [
+//   {
+//     subject: "intensity",
+//     value: 90,
+//     kind: 6,
+//   },
+//   {
+//     subject: "speed",
+//     value: 200,
+//     kind: 5,
+//   },
+//   {
+//     subject: "strength",
+//     value: 50,
+//     kind: 4,
+//   },
+//   {
+//     subject: "endurance",
+//     value: 120,
+//     kind: 2,
+//   },
+//   {
+//     subject: "energy",
+//     value: 80,
+//     kind: 1,
+//   },
+//   {
+//     subject: "cardio",
+//     value: 200,
+//     kind: 1,
+//   },
+// ];
 
-export default function GraphRadar() {
+export default function GraphPerformance({data:{data}}) {
+
+  const formatActivity = (kind) => {
+    let activitiInFrench;
+    switch (kind) {
+      case "cardio":
+        activitiInFrench = "Cardio";
+        break;
+      case "energy":
+        activitiInFrench = "Énergie";
+        break;
+      case "endurance":
+        activitiInFrench = "Endurance";
+        break;
+      case "strength":
+        activitiInFrench = "Force";
+        break;
+      case "speed":
+        activitiInFrench = "Vitesse";
+        break;
+      case "intensity":
+        activitiInFrench = "Intensité";
+        break;
+      default:
+        activitiInFrench = "";
+        break;
+    }
+    return activitiInFrench;
+  };
+
   return (
-    <div className="radarWrapper">
-      <ResponsiveContainer width="100%" height="100%">
+    <div className="RadarContainer">
+      <ResponsiveContainer
+        className="containerRadar"
+        width="100%"
+        height="100%"
+        padding={5}
+      >
         <RadarChart
-          className="radar"
           cx="50%"
           cy="50%"
-          outerRadius={90}
-          width={258}
-          height={263}
+          innerRadius="10%"
+          outerRadius="70%"
           data={data}
         >
-          <PolarGrid />
-          <PolarAngleAxis dataKey="subject" />
+          <PolarGrid radialLines={false} />
+          <PolarAngleAxis
+            dataKey="kind"
+            tickLine={false}
+            tickFormatter={(kind) => formatActivity(kind)}
+            tick={{ fontSize: 12, fontWeight: 500 }}
+            stroke="white"
+            dy={4}
+            radius="100"
+          />
 
           <Radar
-            dataKey="kind"
-            fill="#FF0101B2"
+            name="Performance"
+            dataKey="value"
+            fill="#e60000"
             fillOpacity={0.7}
           />
         </RadarChart>
--- sportsee/src/pages/Profil/index.jsx
+++ sportsee/src/pages/Profil/index.jsx
@@ -9,18 +9,11 @@ import ProteinesIcon from "../../assets/protein-icon.svg";
 import CarbIcon from "../../assets/carbs-icon.svg";
 import FatIcon from "../../assets/fat-icon.svg";
 
-
 // import GRAPHS as component
 import GraphKpiScore from "../../components/GraphsRecharcts/KPIScore";
 import GraphPoids from "../../components/GraphsRecharcts/GraphPoids";
-import GraphObjectif from "../../components/GraphsRecharcts/Objectif"
-// import GraphRadar from "../../components/GraphsRecharcts/Radar";
-
-
-// import GraphObjectif from "../../components/Objectif";
-
-
-
+import GraphObjectif from "../../components/GraphsRecharcts/Objectif";
+import GraphPerformance from "../../components/GraphsRecharcts/Radar";
 
 import "../../styles/profil.css";
 
@@ -41,7 +34,7 @@ function Profil() {
   const [userActivity, setUserActivity] = useState({});
   const [userAverageSessions, setUserAverageSessions] = useState({});
   const [userPerformance, setUserPerformance] = useState({});
-  
+
   console.log(user);
   console.log(userActivity);
   console.log(userAverageSessions);
@@ -55,7 +48,9 @@ function Profil() {
     const datas = new FetchData();
     datas.getInfo(userId).then((data) => setUser(data));
     datas.getActivity(userId).then((data) => setUserActivity(data));
-    datas.getAverageSessions(userId).then((data) => setUserAverageSessions(data));
+    datas
+      .getAverageSessions(userId)
+      .then((data) => setUserAverageSessions(data));
     datas.getPerformance(userId).then((data) => setUserPerformance(data));
   }
 
@@ -70,31 +65,42 @@ function Profil() {
           </h1>
           <p>Félicitation! Vous avez explosé vos objectifs hier 👏 </p>
         </div>
-        <GraphPoids data={userActivity?.sessions}/>
-        
-        <GraphObjectif data={userAverageSessions} />
-        <GraphKpiScore data={user?.todayScore}/>
-        <div className="keyDataWrapper">
-          <KeyData
-            image={CaloriesIcon}
-            title="Calories" value={user?.keyData?.calorieCount}
-            unit="kCal"
-          />
-             <KeyData
-            image={ProteinesIcon}
-            title="Proteines" value={user?.keyData?.proteinCount}
-            unit="g"
-          />
-             <KeyData
-            image={CarbIcon}
-            title="Glucides" value={user?.keyData?.carbohydrateCount}
-            unit="g"
-          />
-             <KeyData
-            image={FatIcon}
-            title="Lipides" value={user?.keyData?.lipidCount}
-            unit="g"
-          />
+        <div className="graphicsAll">
+          <div className="graphicsContainer">
+            <GraphPoids data={userActivity?.sessions} />
+            <div className="graphWrapper">
+              <GraphObjectif data={userAverageSessions} />
+              <GraphPerformance data={userPerformance} />
+              <GraphKpiScore data={user?.todayScore} />
+            </div>
+          </div>
+
+          <div className="keyDataWrapper">
+            <KeyData
+              image={CaloriesIcon}
+              title="Calories"
+              value={user?.keyData?.calorieCount}
+              unit="kCal"
+            />
+            <KeyData
+              image={ProteinesIcon}
+              title="Proteines"
+              value={user?.keyData?.proteinCount}
+              unit="g"
+            />
+            <KeyData
+              image={CarbIcon}
+              title="Glucides"
+              value={user?.keyData?.carbohydrateCount}
+              unit="g"
+            />
+            <KeyData
+              image={FatIcon}
+              title="Lipides"
+              value={user?.keyData?.lipidCount}
+              unit="g"
+            />
+          </div>
         </div>
       </div>
     </div>
