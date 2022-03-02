dcf960f1a99e6a32b97665b470bc83428e9b0e07
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-03-02 14:44:43 +0100

    Create fetchData.jsx
    fetch data mocked or api

---
+++ sportsee/src/fetchData/fetchData.jsx
@@ -0,0 +1,34 @@
+// make class for user
+
+class User {
+  id;
+  userInfos;
+  todayScore;
+  keyData;
+
+  constructor(data) {
+    this.userId = data.userId;
+    this.userInfos = data.userInfos;
+    this.todayScore = data.todayScore;
+    this.keyData = data.keyData;
+  }
+}
+
+// fetch mocked DATA for user
+
+export default class FetchData {
+
+
+  async getInfo(userId) {
+    try {
+      //MOCKED DATA
+      const response = await fetch("../data/" + userId + ".json");
+      //API DATA
+      // const response = await fetch("http://localhost:3000/user/" + userId);
+      const data = await response.json();
+      return new User(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
