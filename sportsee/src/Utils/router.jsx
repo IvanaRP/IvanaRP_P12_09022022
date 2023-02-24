f3a9c8129d65da37061093a443cee4ee49135f1e
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2023-02-24 20:24:00 +0100

    Update router.jsx

--- sportsee/src/Utils/router.jsx
+++ sportsee/src/Utils/router.jsx
@@ -1,4 +1,4 @@
-export const Route_Home = "/home";
+export const Route_Home = "/IvanaRP_P12_09022022";
 export const Route_Profil = "/user/:id";
 export const Route_Settings = "/settings";
 export const Route_Communaute = "/community";
